91e119ac519a6f3d5708901bb884cd3b49390eab
Dao Chi Minh <1312359@student.hcmus.edu.vn>
2017-10-12 05:01:56 +0000

    Init

--- initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/google'                        # required
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:     ENV['gg_access_id'],
-    google_storage_secret_access_key: ENV['gg_access_key']
-  }
-  config.fog_directory = 'cubeblogminhminh'
-end
